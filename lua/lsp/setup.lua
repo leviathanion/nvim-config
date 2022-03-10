94a0fb8623a80f8b6704d765b144d3f0fa60daa3
leviathan <zhu.quntao@foxmail.com>
2022-03-10 12:18:34 +0800

    Update setup.lua
    调整安装和配置顺序，修复警告

--- lua/lsp/setup.lua
+++ lua/lsp/setup.lua
@@ -1,18 +1,3 @@
--- nvim-lspconfig config
--- List of all pre-configured LSP servers:
--- github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
-local servers = {'ccls', 'pyright', "jdtls", "yamlls", "sumneko_lua", 'html', 'tsserver', 'bashls', 'cssls', 'texlab' }
-for _, lsp in pairs(servers) do
-    require('lspconfig')[lsp].setup {
-        on_attach = on_attach,
-        flags = {
-            -- This will be the default in neovim 0.7+
-            debounce_text_changes = 150
-          }
-    }
-end
-
-
 local lsp_installer = require "nvim-lsp-installer"
 
 lsp_installer.settings {
@@ -45,6 +30,20 @@ for _, name in pairs(servers) do
     print("Installing " .. name)
     server:install()
   end
+end
+
+-- nvim-lspconfig config
+-- List of all pre-configured LSP servers:
+-- github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+local servers = {'ccls', 'pyright', "jdtls", "yamlls", "sumneko_lua", 'html', 'tsserver', 'bashls', 'cssls', 'texlab' }
+for _, lsp in pairs(servers) do
+    require('lspconfig')[lsp].setup {
+        on_attach = on_attach,
+        flags = {
+            -- This will be the default in neovim 0.7+
+            debounce_text_changes = 150
+          }
+    }
 end
 
 lsp_installer.on_server_ready(function(server)
