12a6b448b8450e5e055697aa63b54925d19727c5
leviathan <1041281842@qq.com>
2023-02-19 19:54:59 +0800

    添加自动命令

--- init.lua
+++ init.lua
@@ -1,15 +1,15 @@
 local neovide_config = function()
-	vim.cmd([[set guifont=FiraCode\ NF:h13]])
-	vim.g.neovide_refresh_rate = 120
-	vim.g.neovide_cursor_vfx_mode = "railgun"
-	vim.g.neovide_no_idle = true
-	vim.g.neovide_cursor_animation_length = 0.03
-	vim.g.neovide_cursor_trail_length = 0.05
-	vim.g.neovide_cursor_antialiasing = true
-	vim.g.neovide_cursor_vfx_opacity = 200.0
-	vim.g.neovide_cursor_vfx_particle_lifetime = 1.2
-	vim.g.neovide_cursor_vfx_particle_speed = 20.0
-	vim.g.neovide_cursor_vfx_particle_density = 5.0
+    vim.cmd([[set guifont=FiraCode\ NF:h13]])
+    vim.g.neovide_refresh_rate = 120
+    vim.g.neovide_cursor_vfx_mode = "railgun"
+    vim.g.neovide_no_idle = true
+    vim.g.neovide_cursor_animation_length = 0.03
+    vim.g.neovide_cursor_trail_length = 0.05
+    vim.g.neovide_cursor_antialiasing = true
+    vim.g.neovide_cursor_vfx_opacity = 200.0
+    vim.g.neovide_cursor_vfx_particle_lifetime = 1.2
+    vim.g.neovide_cursor_vfx_particle_speed = 20.0
+    vim.g.neovide_cursor_vfx_particle_density = 5.0
 end
 
 neovide_config()
@@ -17,3 +17,4 @@ require("core.pack")
 require("core.theme")
 require("core.options")
 require("core.keymaps")
+require("core.autocmds")
