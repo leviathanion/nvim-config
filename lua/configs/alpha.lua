b28ba833f596e99607443be176eab60e6a64b5f4
leviathan <1041281842@qq.com>
2023-04-04 10:49:14 +0800

    修改初始界面，添加插件数和加载速度显示

--- lua/configs/alpha.lua
+++ lua/configs/alpha.lua
@@ -6,37 +6,72 @@ function M.config()
         vim.notify("没有找到 alpha")
         return
     end
-    local dashboard = require'alpha.themes.dashboard'
+    local dashboard = require 'alpha.themes.dashboard'
     dashboard.section.header.val = {
-        [[MMMMMMMMMMMMMMMMMWWWWWMW0kKKNMMMMMMMMMMM]],
-        [[MMMMMMMMMMMMMMMWXkoodddl'.'oXMMMMMMMMMMM]],
-        [[MMMMMMMMMMMMWXko;...   ....cxKWMMMMMMMMM]],
-        [[MMMMMMMMMWWNk,.  .  .........lNMMMMMMMMM]],
-        [[MMMMMMMMM0o:.     .........'.lNMMMMMMMMM]],
-        [[MMMMMMMMNd. .. .  ....';;...;kNMMWWNNWMM]],
-        [[MMMMMMMWk,...........,lxxocoKWWNNNNXXNWM]],
-        [[MMMMMMMWk' .........,ckOkO0OXWNNNWWWWMMM]],
-        [[MMMMMMMNO;.. .  ....,lO0OO0OKNNWWMMMMMMM]],
-        [[MMMMMMWx'. .  . .:::clxkOOOOXWMMMMMMMMMM]],
-        [[MMMMMWKo;..  .   .':loooxk0KNWMMMMMMMMMM]],
-        [[MMMMMMWNN0o;...',;codxdd0XNMMMMMMMMMMMMM]],
-        [[MMMMMMMMMMW0lldkkOOOOO00XK0XWMMMMMMMMMMM]],
-        [[MMMMMMMMMWk;..';coxOO0KNXKXWMMMMMMMMMMMM]],
-        [[MMMMMMMMXl..   ...';:lkXKKNMMMMMMMMMMMMM]],
-        [[MMMMMMM0;... .........,oxOWMMMMMMMMMMMMM]],
-        [[MMMN0ddxxxxxxxxxxxxxxxxxkkkkOKWMMMMMMMMM]],
+        [[⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⠏⠵⠿⠿⠟⠋⠀⠁⢀⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿]],
+        [[⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠸⠿⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿]],
+        [[⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿]],
+        [[⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⠛⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿]],
+        [[⣿⣿⣿⣿⣿⣿⣿⣿⣿⡧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿]],
+        [[⣿⣿⣿⣿⣿⣿⣿⣿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⡆⢣⣀⠰⢾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿]],
+        [[⣿⣿⣿⣿⣿⣿⣿⣿⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢤⣿⣴⣿⣿⣿⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿]],
+        [[⣿⣿⣿⣿⣿⣿⣿⡿⠿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⣿⢿⡯⢸⣿⣷⣿⣿⣿⣿⣿⣿⣿⣿⣿]],
+        [[⣿⣿⣿⣿⣿⣿⣿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠻⣿⣿⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿]],
+        [[⣿⣿⣿⣿⣿⣿⣟⣡⣤⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣤⣴⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿]],
+        [[⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⡂⠀⠀⠀⠀⠀⠀⣠⣤⣄⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿]],
+        [[⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠀⠀⠀⠐⠲⣿⣿⣟⣽⣾⣿⣿⠃⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿]],
+        [[⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠃⠀⠀⠀⠀⠀⠀⠈⠛⢿⣿⣿⡿⣣⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿]],
+        [[⣿⣿⣿⣿⣿⣿⣿⣿⠟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢻⣷⢌⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿]],
+        [[⣿⣿⣿⣿⣿⣿⣿⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿]],
+        [[⣿⣿⣿⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿]]
     }
+    dashboard.section.header.opts.hl = "Type"
     dashboard.section.buttons.val = {
-        dashboard.button( "SPC c n", "  New file" , ":ene <BAR> startinsert <CR>"),
-        dashboard.button( "SPC f f", "  Find file", ":Telescope find_files<CR>"),
-        dashboard.button( "SPC f r", "  Recent"   , ":Telescope oldfiles<CR>"),
-        dashboard.button( "SPC f g", "  Find word", ":Telescope live_grep<CR>"),
-        dashboard.button( "q", "  Quit NVIM" , ":qa<CR>"),
+        dashboard.button("SPC c n", "  New file", ":ene <BAR> startinsert <CR>"),
+        dashboard.button("SPC f f", "  Find file", ":Telescope find_files<CR>"),
+        dashboard.button("SPC f r", "  Recent", ":Telescope oldfiles<CR>"),
+        dashboard.button("SPC f g", "  Find word", ":Telescope live_grep<CR>"),
+        dashboard.button("q", "  Quit NVIM", ":qa<CR>"),
     }
-    local handle = io.popen('fortune')
-    local fortune = handle:read("*a")
-    handle:close()
-    dashboard.section.footer.val = fortune
-    alpha.setup ( dashboard.opts )
+    local function footer()
+        local stats = require("lazy").stats()
+        local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+        return "   Have Fun with neovim"
+            .. "   v"
+            .. vim.version().major
+            .. "."
+            .. vim.version().minor
+            .. "."
+            .. vim.version().patch
+            .. "   "
+            .. stats.count
+            .. " plugins in "
+            .. ms
+            .. "ms"
+    end
+    dashboard.section.footer.val = footer()
+    dashboard.section.footer.opts.hl = "Function"
+    local head_butt_padding = 1
+    local occu_height = #dashboard.section.header.val + 2 * #dashboard.section.buttons.val + head_butt_padding
+    local header_padding = math.max(0, math.ceil((vim.fn.winheight("$") - occu_height) * 0.25))
+
+    dashboard.config.layout = {
+        { type = "padding", val = header_padding },
+        dashboard.section.header,
+        { type = "padding", val = head_butt_padding },
+        dashboard.section.buttons,
+        dashboard.section.footer,
+    }
+
+    alpha.setup(dashboard.opts)
+
+    vim.api.nvim_create_autocmd("User", {
+        pattern = "LazyVimStarted",
+        callback = function()
+            dashboard.section.footer.val = footer()
+            pcall(vim.cmd.AlphaRedraw)
+        end,
+    })
 end
+
 return M
