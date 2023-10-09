b552fcd3b67539b3c0236fcb80e8a97f204ac74e
leviathan <1041281842@qq.com>
2023-10-09 15:13:04 +0800

    更新新版indent-blankline配置

--- lua/configs/indent-blankline.lua
+++ lua/configs/indent-blankline.lua
@@ -1,6 +1,6 @@
 local M = {}
 function M.config()
-    local status, indent_blankline = pcall(require, "indent_blankline")
+    local status, indent_blankline = pcall(require, "ibl")
     if not status then
         vim.notify("没有找到 indent_blankline")
         return
@@ -8,11 +8,11 @@ function M.config()
     -- indent_blankline config
     indent_blankline.setup {
         -- 显示当前所在区域
-        show_current_context = true,
+        -- show_current_context = true,
         -- 显示当前所在区域的开始位置
-        show_current_context_start = true,
+        -- show_current_context_start = true,
         -- 显示行尾符
-        show_end_of_line = true,
+        -- show_end_of_line = true,
     }
 end
 
